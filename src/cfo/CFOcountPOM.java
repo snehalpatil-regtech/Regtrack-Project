3b52898cacdbf297ffd55ce3c9b4e9e82769642a
Shital Bhoyar <149401577+shitalbhoyar-regtech@users.noreply.github.com>
2025-05-18 19:51:20 +0530

    Update CFOcountPOM.java

--- src/cfo/CFOcountPOM.java
+++ src/cfo/CFOcountPOM.java
@@ -397,7 +397,7 @@ public class CFOcountPOM extends BasePage
 	
 	public static WebElement readCompliancesInternal( )		//Method for reading Compliances value on Dashboard
 	{
-		compliances = getDriver().findElement(By.xpath("//*[@id='ContentPlaceHolder1_divCompliancesCount']"));
+		compliances = getDriver().findElement(By.xpath("//*[@id='ContentPlaceHolder1_divCompliancesCounts']"));
 		return compliances;//*[@id="ContentPlaceHolder1_divCompliancesCount"]
 	}      
 	
